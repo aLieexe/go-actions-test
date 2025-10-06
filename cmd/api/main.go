d93e15ed78b98cef05c809796a0285a299901a1d
aLieexe <rizky.alay@gmail.com>
2025-10-07 06:53:37 +0700

    a little setup for CD test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,59 +1,41 @@
 package main
 
 import (
-	"log/slog"
+	"fmt"
+	"log"
 	"net/http"
-	"os"
 	"time"
-
-	"jwt-golang/internal/auth"
-	"jwt-golang/internal/common"
-	internalHttp "jwt-golang/internal/http"
-	"jwt-golang/internal/models"
-	"jwt-golang/internal/services"
 )
 
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	mux := http.NewServeMux()
 
-	config, err := common.Load(".env")
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	pg, err := services.ConnectPostgres(config.DatabaseURL)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-
-	newModels := models.NewModels(pg)
-	app := &common.Application{
-		Logger: logger,
-		Config: config,
-	}
-
-	jwtProvider := auth.NewJwtProvider(
-		config.AccessTokenSecret,
-		config.RefreshTokenSecret,
-		time.Duration(config.AccesssTokenTTL),
-		time.Duration(config.RefreshTokenTTL),
-	)
-	authManager := auth.NewAuthManager(jwtProvider)
-
-	newHandlers := internalHttp.NewHandlers(newModels, *authManager, app)
+	mux.Handle("/api/", apiHandler{})
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		_, err := fmt.Fprintf(w, "ITS ALIVEEE LOOOLL!")
+		if err != nil {
+			fmt.Println("I dont even know this can error")
+		}
+	})
 
 	server := &http.Server{
-		Addr:         ":" + config.Port,
-		Handler:      internalHttp.Routes(&newHandlers, *authManager, app),
-		IdleTimeout:  time.Second,
-		ReadTimeout:  5 * time.Second,
+		Addr:         ":5500",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		IdleTimeout:  60 * time.Second,
 	}
 
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
-		logger.Error(err.Error())
+		log.Println(err)
 	}
-
-	os.Exit(1)
 }
